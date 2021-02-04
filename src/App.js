e6f463d70a531c55ae416fedd47a29e8a3d7a475
ArthurWSL <arthurhcbp@gmail.com>
2021-02-04 15:52:14 -0300

    testing basic styled components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SubTitle from './components/SubTitle';
+import Title from './components/Title';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title>titulado</Title>
+        <SubTitle> sub titulado </SubTitle>
+        <h1>TESTE123</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
